8c44466a44f4bd24e724fd2e7fba056420dbadae
Noidea22 <mathewsteenwinkel@gmail.com>
2023-03-11 17:08:45 -0700

    created login page

---
+++ routes/login.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router  = express.Router();
+//const userQueries = require('../db/schema/05_widgets');
+
+router.get("/", (req, res) => {
+  res.render('login')
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -30,14 +30,16 @@ app.use(express.static('public'));
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
-const usersRoutes = require('./routes/users');
+const loginRoute = require("./routes/login");
+//const usersRoutes = require('./routes/users');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
+app.use('/login', loginRoute);
+//app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
